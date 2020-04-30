00f0c5d4fcdd188b725a0a3035b08730b68ac2d2
Devone Austin <devone.austin@gmail.com>
2020-04-30 23:17:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,22 @@
 class Triangle
   attr_accessor :a, :b, :c
 
+#Each argument is a length of one of the three sides of the triangle.
+
   def initialize a, b, c
     @a = a
     @b = b
     @c = c
+  end
+
+  def kind
+    if a = b && c => 0
+      :isosceles
+    
+  end
+
+  class TriangleError < StandardError
+    
   end
 
 end
