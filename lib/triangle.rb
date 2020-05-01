b4055b4c27f2936356fd146bb7f2db0e6a5fa34c
Devone Austin <devone.austin@gmail.com>
2020-05-01 00:13:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,11 +12,11 @@ class Triangle
   def kind
     if a <= 0 || b <= 0 || c <= 0 ||
       a + b <= c || b + c <= a || a + c <= b
-        begin
-          raise TriangleError
-        rescue TriangleError => error
-            puts error.message
-          end
+    #  begin
+        raise TriangleError
+    #  rescue TriangleError => error
+    #      puts error.message
+    #  end
     elsif a == b && b == c
       :equilateral
     elsif a == b || c == b || a == c
